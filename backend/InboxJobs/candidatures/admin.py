a337e1ab645e177bc07653470b9515eeb174bb26
Robin Landraud <robin.landraud@epitech.eu>
2025-10-04 19:16:33 +0200

    [RM] email and candidatures back apps

--- backend/InboxJobs/candidatures/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Candidature
-
-@admin.register(Candidature)
-class CandidatureAdmin(admin.ModelAdmin):
-    list_display = ('position', 'company', 'platform', 'status', 'user', 'date')
-    list_filter = ('platform', 'status')
-    search_fields = ('position', 'company')
--- backend/InboxJobs/candidatures/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Candidature
-
-class CandidatureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Candidature
-        fields = '__all__'
