7ea3a810e6a49fa9fa5d6ec282d90c8ce95ef0a3
Robin Landraud <72014398+RobinLandraud@users.noreply.github.com>
2025-12-28 10:48:34 +0100

    [UPDATE] add register page in router

--- frontend/inboxjobs/src/routes.tsx
+++ frontend/inboxjobs/src/routes.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
   </Routes>
 );
 
